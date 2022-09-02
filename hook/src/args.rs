0e2f568d68102eab711a0bedfa84fb9d567395d0
Suess <justin.suess@westpoint.edu>
2022-09-02 09:20:17 -0400

    added the arguments and actix server

--- hook/src/args.rs
+++ hook/src/args.rs
@@ -4,5 +4,9 @@ use clap::Parser;
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
     #[clap(short, long)]
-    c2c_address : String
+    pub c2c_address : Option<String>,
+    #[clap(long, default_value = "0.0.0.0")]
+    pub bind_address : String,
+    #[clap(short, long, default_value = "10997")]
+    pub bind_port : u16
 }
--- hook/src/main.rs
+++ hook/src/main.rs
@@ -1,5 +1,15 @@
 mod args;
-fn main() {
-    //let args = args::Args::parse();
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+use clap::Parser;
+
+#[tokio::main] 
+async fn main() -> std::io::Result<()> {
+    let opts = args::Args::parse();
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+    })
+    .bind((opts.bind_address, opts.bind_port))?
+    .run()
+    .await
 }
