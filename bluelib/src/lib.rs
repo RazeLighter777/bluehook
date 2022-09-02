5d69578776e401e5c380502f2b6227c01e857253
Suess <justin.suess@westpoint.edu>
2022-09-02 09:32:27 -0400

    Added shared library with OS detection

---
+++ bluelib/src/lib.rs
@@ -0,0 +1,21 @@
+
+pub enum Platform {
+    WINDOWS,
+    LINUX,
+    FREEBSD
+}
+
+#[cfg(target_os = "windows")]
+pub fn get_platform() -> Platform {
+    Platform::WINDOWS
+}
+#[cfg(target_os = "linux")]
+pub fn get_platform() -> Platform {
+    Platform::LINUX
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+}
--- commander/src/main.rs
+++ commander/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+use clap::Parser;
+
+#[tokio::main] 
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+    })
+    .bind(("0.0.0.0", 10996))?
+    .run()
+    .await
 }
